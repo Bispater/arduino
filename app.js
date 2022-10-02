168f3a884b98f7afa47f4908f2522d7ee5533798
JORGE MARQUEZ LOPEZ <jmarquez@MacBook-Pro-de-JORGE.local>
2022-10-01 21:26:46 -0300

    first change

---
+++ app.js
@@ -0,0 +1,27 @@
+const { SerialPort } = require('serialport')
+const { ReadlineParser } = require('@serialport/parser-readline')
+const axios = require('axios');
+
+
+const port = new SerialPort({
+    path: '/dev/cu.usbmodem14101',
+    baudRate: 9600}
+)
+
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
+
+parser.on('data', (line)=>{
+    axios.post('https://77dd-186-104-190-226.sa.ngrok.io/_tesscloud/organization/mwx41f4bgK/_tesscontrol/personal_notebook_control/new', {
+        name_floor: 'Fred',
+      },{headers: {
+        "Content-type": "application/json", "X-Parse-Applitcation-Id": 'myAppId', "X-Parse-Master-API-Key": 'myMasterKey'
+      }})
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    console.log('Arduino dice: ' + line)
+    port.write('Era una vez ')
+})
