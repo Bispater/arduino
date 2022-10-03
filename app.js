fdbdc7b00520a93853adc168be94d86f89a32ecf
JORGE MARQUEZ LOPEZ <jmarquez@MacBook-Pro-de-JORGE.local>
2022-10-03 12:04:46 -0300

    minix ready

--- app.js
+++ app.js
@@ -2,24 +2,32 @@
 const { SerialPort } = require('serialport')
 const { ReadlineParser } = require('@serialport/parser-readline')
 const axios = require('axios');
+var Netcat = require('node-netcat');
 
 //Socket
 var net = require('net');
-var port = 8107; // PORT FOR CONNECTING WITH THE IP SERVER OF MINIX.
-var host = '192.168.100.7'; // IP FOR CONNECTING WITH THE IP SERVER OF MINIX.
-
+const { close } = require('fs');
+var port = 8000; // PORT FOR CONNECTING WITH THE IP SERVER OF MINIX.
+//192.168.100.19
+// RINNO MINIX 192.168.8.101
+var host = '192.168.64.70'; // IP FOR CONNECTING WITH THE IP SERVER OF MINIX.
 
 const Arduinoport = new SerialPort({
-  /* PATH FROM MAC
+  /* 
+  PATH FROM MAC
   path: '/dev/cu.usbmodem14101',
+  PATH FROM RASPBERRY
+  path: '/dev/ttyACM0'
   */
   path: '/dev/cu.usbmodem14101',
   baudRate: 9600}
 )
 
-const parser = Arduinoport.pipe(new ReadlineParser({ delimiter: '\r\n' }))
+const parser = Arduinoport.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+// client = Netcat.client(port, host);
 
-parser.on('data', (line)=>{
+parser.on('data', async (line)=>{
+
   /* POST FOR HTML SERVER;
     axios.post('https://77dd-186-104-190-226.sa.ngrok.io/_tesscloud/organization/mwx41f4bgK/_tesscontrol/personal_notebook_control/new', {
         name_floor: 'Fred',
@@ -33,20 +41,67 @@ parser.on('data', (line)=>{
         console.log(error);
       });*/
       // local client name
+  var s = require('net').Socket();
+  s.connect(port, host);
+  s.write('test\n\n');
+  s.on('data', function(d){
+    console.log(d.toString());
+  });
+  s.end();
+  /*
+      console.log('Move!', line)
+      var scan = Netcat.portscan();
+
+      scan.run(host, port, function (err, res) {
+        console.log(err, res);	
+      });  
+  */
+      // var client = Netcat.client(port, host);
+      
+      /*client.on('open', function () {
+        console.log('connect');
+        client.send('1' + '\n');
+        
+      });
+
+       client.on('data', function (data) {
+        console.log(data.toString('ascii'));
+        client.send('Goodbye!!!', true);
+      });
+ 
+      client.on('error', function (err) {
+        console.log(err);
+      });   
+      
+      client.on('close', function () {
+        console.log('close');
+      });
+*/
+      // client.send('1' + '\n');
+      // client.start();
+
+    /* var client = new net.Socket();
+    client.connect(port, host, function() {
+      console.log(`sending to server: a.random.test`)
+      client.write(`1\n`)  
+      client.setTimeout(1000);                        
+    }); */
+    
+    
+
+  /*
     console.log("=============================")
-    var Hobbits = getConnection("Hobbits");
-
-    // message
-    writeData(Hobbits,"Chevrolet Video");
-
-    /*
-    console.log('Arduino dice: ' + line)
-    Arduinoport.write('Era una vez ')
+    try {
+      var Hobbits = getConnection("Hobbits");
+      // message
+      writeData(Hobbits,"1");
+    }catch(e){
+      console.log(e);
+    }
     */
-})
+  })
 
-
-
+/*
 function getConnection(connName){
 	var client = net.connect({port,host},function(){
 		console.log(connName + ' Connected:');
@@ -55,36 +110,42 @@ function getConnection(connName){
 		this.setTimeout(500);
 		this.setEncoding('utf8');
 
-        //log with de data that we are sending
+    //log with de data that we are sending
 		this.on('data',function(data){
 			console.log(data.toString());
 			this.end();
 		});
-        //connection end
+    //connection end
 		this.on('end',function(){
 			console.log(connName + 'Client disconnected');
 		});
-        //error displayed on console log
+    //error displayed on console log
 		this.on('error',function(err){
 			console.log('Socket Error: ',JSON.stringify(err));
-            console.log("=============================")
+      console.log("=============================")
 
 		});
-        //socket isn't responding
+    //socket isn't responding
 		this.on('timeout',function(){
-			console.log('Socket Timed Out');
-            console.log("=============================")
+    console.log('Socket Timed Out');
+    console.log("=============================")
+    try{      
+      //client.destroy();
+    }catch(e){
+      console.log(e);
+    }
 
 		});
-        //socket closed
+    //socket closed
 		this.on('close',function(){
 			console.log('Socket Closed');
-            console.log("=============================")
+      console.log("=============================")
 
 		});
 	});
-	return client;
+  return client;
 }
+
 function writeData(socket,data){
 	var success = !socket.write(data);
 	if(!success){
@@ -95,3 +156,4 @@ function writeData(socket,data){
 		})(socket,data);
 	}
 }
+*/
