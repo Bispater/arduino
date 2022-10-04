066c8994b72a6799014881c64856b6e1544bf48c
JORGE MARQUEZ LOPEZ <jmarquez@MacBook-Pro-de-JORGE.local>
2022-10-04 02:11:47 -0300

    minix conf

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 
 const { SerialPort } = require('serialport')
 const { ReadlineParser } = require('@serialport/parser-readline')
-const axios = require('axios');
-var Netcat = require('node-netcat');
+//const axios = require('axios');
 
-//Socket
+//SOCKET: 
 var net = require('net');
 const { close } = require('fs');
-var port = 8000; // PORT FOR CONNECTING WITH THE IP SERVER OF MINIX.
-//192.168.100.19
-// RINNO MINIX 192.168.8.101
-var host = '192.168.8.104'; // IP FOR CONNECTING WITH THE IP SERVER OF MINIX.
+var port = 8000;            // THE APP DEFINE THIS PORT. 
+var host = '192.168.8.104'; // IP FOR CONNECTING WITH THE (IP SERVER OF MINIX).
 
 const Arduinoport = new SerialPort({
   /* 
-  PATH FROM MAC
-  path: '/dev/cu.usbmodem14101',
-  PATH FROM RASPBERRY
+  PATH FROM MAC: path: '/dev/cu.usbmodem14101',
   */
+
+  //PATH FROM RASPBERRY:
   path: '/dev/ttyACM0',
-  //path: '/dev/cu.usbmodem14101',
   baudRate: 9600}
 )
 
@@ -40,124 +36,17 @@ parser.on('data', async (line)=>{
       .catch(function (error) {
         console.log(error);
       });*/
-      // local client name
+  // local client name
   try{
-  var s = require('net').Socket();
-  s.connect(port, host);
-  s.write('1\n\n');
-  s.on('data', function(d){
+    var s = require('net').Socket();
+    s.connect(port, host);
+    s.write('1\n\n');
+    s.on('data', function(d){
     console.log(d.toString());
   });
   s.end();
   }catch(e){
     console.log(e);
-  }
-  /*
-      console.log('Move!', line)
-      var scan = Netcat.portscan();
+  }  
+})
 
-      scan.run(host, port, function (err, res) {
-        console.log(err, res);	
-      });  
-  */
-      // var client = Netcat.client(port, host);
-      
-      /*client.on('open', function () {
-        console.log('connect');
-        client.send('1' + '\n');
-        
-      });
-
-       client.on('data', function (data) {
-        console.log(data.toString('ascii'));
-        client.send('Goodbye!!!', true);
-      });
- 
-      client.on('error', function (err) {
-        console.log(err);
-      });   
-      
-      client.on('close', function () {
-        console.log('close');
-      });
-*/
-      // client.send('1' + '\n');
-      // client.start();
-
-    /* var client = new net.Socket();
-    client.connect(port, host, function() {
-      console.log(`sending to server: a.random.test`)
-      client.write(`1\n`)  
-      client.setTimeout(1000);                        
-    }); */
-    
-    
-
-  /*
-    console.log("=============================")
-    try {
-      var Hobbits = getConnection("Hobbits");
-      // message
-      writeData(Hobbits,"1");
-    }catch(e){
-      console.log(e);
-    }
-    */
-  })
-
-/*
-function getConnection(connName){
-	var client = net.connect({port,host},function(){
-		console.log(connName + ' Connected:');
-		console.log('  Local = %s:%s',this.localAddress,this.localPort);
-		console.log('  Remote = %s:%s',this.remoteAddress,this.remotePort);
-		this.setTimeout(500);
-		this.setEncoding('utf8');
-
-    //log with de data that we are sending
-		this.on('data',function(data){
-			console.log(data.toString());
-			this.end();
-		});
-    //connection end
-		this.on('end',function(){
-			console.log(connName + 'Client disconnected');
-		});
-    //error displayed on console log
-		this.on('error',function(err){
-			console.log('Socket Error: ',JSON.stringify(err));
-      console.log("=============================")
-
-		});
-    //socket isn't responding
-		this.on('timeout',function(){
-    console.log('Socket Timed Out');
-    console.log("=============================")
-    try{      
-      //client.destroy();
-    }catch(e){
-      console.log(e);
-    }
-
-		});
-    //socket closed
-		this.on('close',function(){
-			console.log('Socket Closed');
-      console.log("=============================")
-
-		});
-	});
-  return client;
-}
-
-function writeData(socket,data){
-	var success = !socket.write(data);
-	if(!success){
-		(function(socket,data){
-			socket.once('drain',function(){
-				writeData(socket,data);
-			});
-		})(socket,data);
-	}
-}
-*/
